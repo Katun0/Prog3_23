606af33cbbbaab28a25567e4e4a03533c9b5ef04
João <joao_katun0@hotmail.com>
2023-04-24 09:59:16 -0300

    Recursividade

---
+++ Recursividade/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Recursividade {
+    static void Main(string[] args) {
+    //Número multiplicado
+        Console.WriteLine("Digite um número:");
+        int num = Convert.ToInt32(Console.ReadLine());
+    // Começa com o primeiro multiplicador    
+        int i = 1; 
+    //Teto de multiplicações
+        Console.WriteLine("Digite até quanto vai a tabuada: ");
+        int limit = Convert.ToInt32(Console.ReadLine());
+    //Função para multiplicar
+        ImprimeTabuada(num, limit, i);
+    }
+
+    static void ImprimeTabuada(int num, int limit, int i) {
+        if (i > limit) //Caso base
+        {
+            Console.WriteLine($"Tauaba Completa");
+        }
+        else if (i <= limit) {
+            Console.WriteLine($"{num} x {i} = {num*i}\r");
+            i++;
+            ImprimeTabuada(num, limit, i); // Chamada recursiva
+        }
+    }
+}
