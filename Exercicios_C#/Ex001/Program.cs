6a99e30d781e3ab18477a4339da3834d9e932958
João V. Surdi <80220242+Katun0@users.noreply.github.com>
2023-06-29 16:09:53 -0300

    Final project commit

---
+++ Exercicios_C#/Ex001/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+Console.WriteLine("Digite um número:");
+int input = Convert.ToInt32(Console.ReadLine());
+
+if (input % 2 == 0)
+{
+    Console.WriteLine($"{input} é PAR");
+}
+else if (input % 2 == 1)
+{
+    System.Console.WriteLine($"{input} é IMPAR");
+}
+else
+{
+    System.Console.WriteLine($"{input} não é um número válido");
+}
